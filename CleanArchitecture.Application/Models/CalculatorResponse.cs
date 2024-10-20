5524c9d54f770dc0a310877d615b26e49dbf9f46
Muhammad Akhtar Khan <makhtarkhan@gmail.com>
2024-10-20 10:20:49 +0500

    updated model

--- CleanArchitecture.Application/Models/CalculatorResponse.cs
+++ CleanArchitecture.Application/Models/CalculatorResponse.cs
@@ -25,16 +25,5 @@ namespace ComplexCalculator.Application.Models
         public DateTime? CreatedOn { get; set; }
 
     }
-    public class CalculatorSumModel
-    {     
-        public int One { get; set; }
-        public int Two { get; set; }
-        public int Three { get; set; }
-        public int Four { get; set; }
-        public int Five { get; set; }
-        public int Six { get; set; }        
-        public int Luozi { get; set; }            
-
-    }
 
 }
---
+++ CleanArchitecture.Application/Models/CalculatorSumModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ComplexCalculator.Application.Models
+{
+    public class CalculatorSumModel
+    {     
+        public int One { get; set; }
+        public int Two { get; set; }
+        public int Three { get; set; }
+        public int Four { get; set; }
+        public int Five { get; set; }
+        public int Six { get; set; }        
+        public int Luozi { get; set; }            
+
+    }
+
+}
